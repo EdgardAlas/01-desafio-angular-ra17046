450d1c2ad80f4b075e52911c07afa47573a2eb76
Edgard Alas <edgardalasu@gmail.com>
2023-03-18 16:29:56 -0600

    Fixing texts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-  constructor() {}
-
+export class HeaderComponent {
   button1 = 'assets/images/icon-menu.svg';
   button2 = 'assets/images/icon-menu-close.svg';
   isOpen = false;
 
-  ngOnInit(): void {}
-
-  ngOnDestroy(): void {}
+  constructor(private makeRender: ChangeDetectorRef) {
+    const mediaQuery = window.matchMedia('(min-width: 768px)');
+    mediaQuery.addEventListener('change', () => {
+      this.isOpen = false;
+      this.makeRender.detectChanges();
+    });
+  }
 
   toggleMenu() {
     this.isOpen = !this.isOpen;
